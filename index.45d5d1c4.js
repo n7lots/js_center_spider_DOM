81277dd62f71a7e7998b6a390f0564c6ffaabed5
Vasyliev Vadym <protocol222333@gmail.com>
2025-09-05 15:27:10 +0300

    make build

---
+++ index.45d5d1c4.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",function(){var e=document.querySelector(".wall"),t=document.querySelector(".spider"),n=(e.clientHeight-t.clientHeight)/2+"px",i=(e.clientWidth-t.clientWidth)/2+"px";t.style.position="absolute",t.style.top=n,t.style.left=i});
+//# sourceMappingURL=index.45d5d1c4.js.map
