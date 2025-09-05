858c10c51ec6d2bc53916286de3caa72e60bc635
Vasyliev Vadym <protocol222333@gmail.com>
2025-09-05 15:26:31 +0300

    fix: add absolute positioning and DOMContentLoaded wrapper for spider centering

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,13 @@
 'use strict';
 
-const field = document.querySelector('.wall');
-const spider = document.querySelector('.spider');
+document.addEventListener('DOMContentLoaded', () => {
+  const field = document.querySelector('.wall');
+  const spider = document.querySelector('.spider');
 
-const calculatedTop = (field.clientHeight - spider.clientHeight) / 2 + 'px';
-const calculatedLeft = (field.clientWidth - spider.clientWidth) / 2 + 'px';
+  const calculatedTop = (field.clientHeight - spider.clientHeight) / 2 + 'px';
+  const calculatedLeft = (field.clientWidth - spider.clientWidth) / 2 + 'px';
 
-spider.style.top = calculatedTop;
-spider.style.left = calculatedLeft;
+  spider.style.position = 'absolute';
+  spider.style.top = calculatedTop;
+  spider.style.left = calculatedLeft;
+});
